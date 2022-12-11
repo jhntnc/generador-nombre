bde7fc128195cf28a88bc23b9b421823ec8b5512
jhntnc <JHONATANCALDERON2104@GMAIL.COM>
2022-12-11 19:18:11 +0000

    repositorio

--- src/app.js
+++ src/app.js
@@ -9,3 +9,27 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our", "you", "to", "main", "be"];
+let adj = ["great", "big", "small", "ligh", "top", "bottom"];
+let noun = ["jogger", "racoon", "windons", "glass", "door", "head", "heart"];
+
+/*function principal(){
+    let prono = Math.floor(Math.random()*5)+1;
+
+    let ad = Math.floor(Math.random()*5)+1;
+
+    let nou = Math.floor(Math.random()*6)+1;
+
+    console.log(principal(pronoun[prono]+adj[ad]+noun[nou]+".com"))
+}*/
+
+window.onload = function principal() {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        console.log(pronoun[i] + adj[a] + noun[b] + ".com");
+      }
+    }
+  }
+};
